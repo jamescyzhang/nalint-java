1f57adfb14fec8862cdf7299bbeb076844ef1649
James Zhang <james.cy.zhang@gmail.com>
2014-03-25 16:59:07 -0700

    Fixed execution output

--- src/nalint/Executor.java
+++ src/nalint/Executor.java
@@ -1,26 +1,29 @@
 package nalint;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Executor
 {
 
 	private static Runtime runtime = Runtime.getRuntime();
-	private static Scanner sc;
+	private static BufferedReader reader;
 
 	public static void exec(String command)
 	{
 		Process p;
+		StringBuffer buffer = new StringBuffer();
 		try
 		{
 			p = runtime.exec(command);
 			p.waitFor();
-			sc = new Scanner(p.getInputStream());
+			reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line = "";
-			while ((line = sc.nextLine()) != null)
+			while ((line = reader.readLine()) != null)
 			{
-				System.out.println(line + "\n");
+				buffer.append(line + "\n");
 			}
 		}
 		catch (Exception e)
@@ -28,6 +31,8 @@ public class Executor
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		System.out.println(buffer.toString());
 
 	}
 
